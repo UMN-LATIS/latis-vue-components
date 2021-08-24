d074279a8b0e6d4ae11700e3f172cb993afc6926
James Johnson <johnsojr@umn.edu>
2021-08-23 21:59:46 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import "./assets/fontawesome-5/css/all.min.css";
+import "./assets/normalize.css";
+import "./assets/main.css";
+import UMNSearch from "./components/UMNSearch.vue";
+</script>
+
+<style></style>
+
+<template>
+  <h1>UMN Components</h1>
+  <div class="component">
+    <h2 class="component__name">UMN Search</h2>
+    <div class="component__body">
+      <UMNSearch />
+    </div>
+  </div>
+</template>
---
+++ src/components/UMNSearch.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="umn-search">
+    <button
+      class="umn-search__toggle-search-form"
+      @click="handleToggleFormClosed"
+    >
+      <i class="fa fa-search"></i>
+    </button>
+    <form class="umn-search__form" :class="searchFormClasses">
+      <label for="umn-search__input" class="visually-hidden">Search</label>
+      <input
+        type="search"
+        id="umn-search__input"
+        value=""
+        placeholder="Search UMN"
+      />
+      <button class="umn-search__button" type="submit">Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      isClosed: false,
+      search: "",
+    });
+
+    const handleToggleFormClosed = () => {
+      console.log("Toggle Form");
+      state.isClosed = !state.isClosed;
+    };
+    const handleUpdate = (event) => (state.search = event.target.value);
+    const handleSubmit = (event) => {
+      console.log("Submit Form");
+    };
+
+    const searchFormClasses = computed(() => ({
+      "umn-search__form--is-closed": state.isClosed,
+    }));
+
+    return {
+      state,
+      handleToggleFormClosed,
+      handleUpdate,
+      handleSubmit,
+      searchFormClasses,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* .umn-search {
+  display: flex;
+} */
+
+.umn-search__input {
+  border-radius: 0.25rem 0 0 0.25rem;
+  border-right: 0;
+}
+.umn-search__button {
+  /* border-left: 0; */
+  border-radius: 0 0.25rem 0.25rem 0;
+}
+
+.umn-search__form--is-closed {
+  display: none;
+}
+</style>
