570f1abd278731e8aff5f011a7b083e4b6a9e10a
James Johnson <johnsojr@umn.edu>
2021-08-24 22:12:51 -0500

    Homepage

---
+++ src/components/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="hero">
+    <div class="container">
+      <div class="hero__image">
+        <img src="imgSrc" alt="imgAlt" />
+      </div>
+      <div class="hero__text"></div>
+    </div>
+  </section>
+</template>
