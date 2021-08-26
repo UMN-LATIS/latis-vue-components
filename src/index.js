c9571293a56656e72cc00ddb459b15aa753789ea
James Johnson <johnsojr@umn.edu>
2021-08-25 21:32:58 -0500

    load globals

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// global stylesheets
+import "./assets/normalize.css";
+import "./assets/fontawesome-5/css/all.min.css";
+import "./assets/main.css";
+
 import { AppHeader } from "./components/AppHeader/index.js";
 import { AppNav, AppNavItem, AppNavSubNav } from "./components/AppNav/index.js";
 import { LoginButton } from "./components/LoginButton/index.js";
